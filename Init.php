20a653698a7fea8cc3392d3cc402fda4487fc74b
dainierr <147769053+dainierr@users.noreply.github.com>
2025-02-12 14:59:32 +0100

    initial  commit

---
+++ Init.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace FacturaScripts\Plugins\WoocommerceSync;
+
+use FacturaScripts\Core\Template\InitClass;
+
+/**
+ * Description of Init
+ *
+ * @author José Antonio Cuello Principal <yopli2000@gmail.com>
+ */
+class Init extends InitClass
+{
+
+    /**
+     * Code to load every time FacturaScripts starts.
+     */
+    public function init(): void
+    {
+    }
+
+    /**
+     * Code to load every time the plugin is enabled or updated.
+     */
+    public function update(): void
+    {
+    }
+
+    /**
+     * Code that is executed when uninstalling a plugin.
+     */
+    public function uninstall(): void
+    {
+    }
+}
